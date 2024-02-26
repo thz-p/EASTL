76384c7a57b96d3bb2dc46f078ce8f8d24de5f22
thz-p <872846303@QQ.COM>
2024-02-26 09:28:50 +0000

    t

--- include/EASTL/unordered_set.h
+++ include/EASTL/unordered_set.h
@@ -1,15 +1,17 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Electronic Arts Inc. All rights reserved.
+// 版权声明
+// 版权所有© Electronic Arts Inc. 保留所有权利。
 ///////////////////////////////////////////////////////////////////////////////
 
 #ifndef EASTL_UNORDERED_SET_H
 #define EASTL_UNORDERED_SET_H
 
+// 包含必要的头文件
 #include <EASTL/internal/config.h>
 #include <EASTL/hash_set.h>
 
 #if defined(EA_PRAGMA_ONCE_SUPPORTED)
-	#pragma once // Some compilers (e.g. VC++) benefit significantly from using this. We've measured 3-4% build speed improvements in apps as a result.
+	#pragma once // 一些编译器（例如 VC++）会从中获益。我们通过测量已经证明在应用程序中可以提高 3-4% 的构建速度。
 #endif
 
 namespace eastl
@@ -17,12 +19,9 @@ namespace eastl
 
 	/// unordered_set 
 	///
-	/// The original TR1 (technical report 1) used "hash_set" to name a hash
-	/// table backed associative container of unique "Key" type objects.  When
-	/// the container was added to the C++11 standard the committee chose the
-	/// name "unordered_set" to clarify that internally the elements are NOT
-	/// sorted in any particular order.  We provide a template alias here to
-	/// ensure feature parity with the original eastl::hash_set.
+	/// 最初的 TR1（技术报告1）使用 "hash_set" 来命名一个基于哈希表的关联容器，用于存储唯一的 "Key" 类型对象。
+	/// 当容器被添加到 C++11 标准中时，委员会选择了名字 "unordered_set" 来澄清内部元素不以任何特定顺序排序的事实。
+	/// 我们在这里提供了一个模板别名，以确保与原始的 eastl::hash_set 具有相同的特性。
 	///
 	#if !defined(EA_COMPILER_NO_TEMPLATE_ALIASES)
 		template <typename Value,
@@ -35,8 +34,7 @@ namespace eastl
 
     /// unordered_multiset 
 	///
-	/// Similar template alias as "unordered_set" except the contained elements
-	/// need not be unique. See "hash_multiset" for more details. 
+	/// 类似于 "unordered_set" 的模板别名，但包含的元素不需要是唯一的。更多细节请参见 "hash_multiset"。
 	///
 	#if !defined(EA_COMPILER_NO_TEMPLATE_ALIASES)
 		template <typename Value,
@@ -51,3 +49,10 @@ namespace eastl
 
 #endif // Header include guard
 
+// 这段代码是一个头文件，定义了两个模板别名 `unordered_set` 和 `unordered_multiset`，它们分别代表哈希集合和哈希多重集合。主要功能包括：
+
+// 1. 提供对哈希集合和哈希多重集合的访问。
+// 2. 允许用户指定自定义的哈希函数、比较函数和分配器。
+// 3. 使用预处理指令防止重复包含，并使用 `#pragma once` 提高编译效率。
+
+// 这样，用户在使用哈希集合和哈希多重集合时，可以通过这两个模板别名方便地进行访问和操作，同时也可以根据需要进行自定义参数配置。
