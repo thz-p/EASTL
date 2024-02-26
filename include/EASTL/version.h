c2bd33b98c1afbca8d993a1122e4176001d0d6d8
thz-p <872846303@QQ.COM>
2024-02-26 08:59:52 +0000

    t

--- include/EASTL/version.h
+++ include/EASTL/version.h
@@ -1,15 +1,19 @@
 /////////////////////////////////////////////////////////////////////////////
-// Copyright (c) Electronic Arts Inc. All rights reserved.
+// 版权声明
+// 版权所有© Electronic Arts Inc. 保留所有权利。
 /////////////////////////////////////////////////////////////////////////////
 
 #ifndef EASTL_VERSION_H
 #define EASTL_VERSION_H
 
+// 包含必要的头文件
 #include <EABase/eabase.h>
+// 如果编译器支持#pragma once指令，则使用#pragma once，确保头文件只被包含一次
 #if defined(EA_PRAGMA_ONCE_SUPPORTED)
 	#pragma once
 #endif
 
+// 包含配置文件
 #include <EASTL/internal/config.h>
 
 #endif
